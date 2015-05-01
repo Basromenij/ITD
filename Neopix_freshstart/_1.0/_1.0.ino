732187dbfd178bdba771ac0ed2024a5b9a588bbf
Basromenij <b.ter.haar.romenij@gmail.com>
2015-05-01 16:37:21 +0200

    verfijnen van rainbow

--- Neopix_freshstart/_1.0/_1.0.ino
+++ Neopix_freshstart/_1.0/_1.0.ino
@@ -2,24 +2,24 @@
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
 
-#define PIN1            3
-#define PIN2            5
-#define teller          8
-#define schakelaar      4
-#define druk            2
+#define PIN1              3
+#define PIN2              5
+#define teller            8
+#define schakelaar        4
+#define druk              2
 
-#define NUMPIXELS1      4
-#define NUMPIXELS2      3
+#define NUMPIXELS1        4
+#define NUMPIXELS2        3
 
-boolean status_energy = 1;
-int stepSize = 1;
-int x = 0;
-int telwaarde = 0;
+boolean status_energy =   1;
+int stepSize =            1;
+int x =                   0;
+int telwaarde =           0;
 
-  int RainbowColor = 0;
-  int ColorChanger = 1;
+int RainbowColor =       100;
+int ColorChanger =       1;
 
-float fadeValue = 150;
+float fadeValue =        150;
 
 Adafruit_NeoPixel pixels1 = Adafruit_NeoPixel(NUMPIXELS1, PIN1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(NUMPIXELS2, PIN2, NEO_GRB + NEO_KHZ800);
@@ -43,12 +43,8 @@ void setup() {
 void loop() {
 
   if (digitalRead(druk) == HIGH) {
-    Serial.println("poepchineeeees");
+    //    Serial.println("poepchineeeees");
     status_energy = 1;
-  }
-
-  if (digitalRead(schakelaar) == HIGH) {
-    Serial.println("schakel doet het");
   }
 
 
@@ -58,46 +54,37 @@ void loop() {
       for (fadeValue ; fadeValue > 0; fadeValue -= stepSize) {
         x = fadeValue;
 
-        Serial.println(digitalRead(druk));
-        pixels1.setPixelColor(0, pixels1.Color(x, x, x)); // sets a white color for neopix with a brightness x
-        pixels1.setPixelColor(1, pixels1.Color(x, x, x)); // sets a white color for neopix with a brightness x
-        pixels1.setPixelColor(2, pixels1.Color(x, x, x)); // sets a white color for neopix with a brightness x
-        pixels2.setPixelColor(0 , 0); // turns off neopix
-        pixels2.setPixelColor(1 , 0); // turns off neopix
-        pixels2.setPixelColor(2 , 0 ); // turns off neopix
-        pixels1.show(); // This sends the updated pixel color to the hardware.
-        pixels2.show(); // This sends the updated pixel color to the hardware.
+        //        Serial.println(digitalRead(druk));
 
-        if (x == 0) {
-          status_energy = 0 ;
-          break;
-          Serial.println("waarde is nu een");
+        for (int i = 0; i < 3; i++) {
+          pixels1.setPixelColor(i, Wheel(x));
+          pixels2.setPixelColor(i, 0); // sets a white color for neopix with a brightness x
+          pixels1.show();
+          pixels2.show();
+          if (x == 0) {
+            status_energy = 0 ;
+            break;
+            //            Serial.println("waarde is nu een");
+          }
+          if ((digitalRead(druk)) == LOW)  {     // turn other led strip on
+            //            Serial.print(" hij komt in de break if");
+            break;
+          }
+          delay(delayval); // Delay for a period of time (in milliseconds).
+
         }
-        if ((digitalRead(druk)) == LOW)  {     // turn other led strip on
-          Serial.print(" hij komt in de break if");
-          break;
-        }
-        delay(delayval); // Delay for a period of time (in milliseconds).
-      
-    }
+      }
     }
 
     if (digitalRead(druk) == HIGH && digitalRead(schakelaar) == LOW) {
       for (fadeValue ; fadeValue > 0; fadeValue -= stepSize) {
         x = fadeValue;
         for (int i = 0; i < 3; i++) {
-          RainbowColor = RainbowColor + ColorChanger;
-   if (RainbowColor == 150) {
-    ColorChanger = -1;
-  }
-  if (RainbowColor == 20) {
-    ColorChanger = 1;
-  }
           pixels1.setPixelColor(i, Wheel(x));
           pixels2.setPixelColor(i, 0); // sets a white color for neopix with a brightness x
           pixels1.show();
           pixels2.show();
-        
+
         }
         if (x == 0) {
           status_energy = 0 ;
@@ -108,7 +95,7 @@ void loop() {
           Serial.print(" hij komt in de break if");
           break;
         }
-        delay(delayval);
+        delay(delayval2);
       }
     }
   }
@@ -133,26 +120,33 @@ void loop() {
   }
   Serial.print("waarde van x: ");
   Serial.println(x);
-  
-  // changing color of the rainbow
-   
-  
 }
+// changing color of the rainbow
+
 
 uint32_t Wheel(byte intensity) {
-  
-  float mappedintensity = map(intensity, 0, 150, 0, 25) * 0.04;
 
-  Serial.println(mappedintensity);
+  RainbowColor = RainbowColor + ColorChanger;
+  if (RainbowColor == 150) {
+    ColorChanger = -1;
+  }
+  if (RainbowColor == 0) {
+    ColorChanger = 1;
+  }
 
-  
-  if(RainbowColor < 35) {
-   return pixels1.Color(mappedintensity*(255-RainbowColor), mappedintensity*RainbowColor * 3, 0);
-  } else if(RainbowColor < 100) {
-   return pixels1.Color(mappedintensity*(255-RainbowColor * 3), 0, mappedintensity*RainbowColor * 3);
+  float mappedintensity = map(intensity, 0, 150, 0, 10) * 0.01;
+
+  Serial.print(mappedintensity);
+  Serial.print("       ");
+  Serial.println(RainbowColor);
+
+  if (RainbowColor < 35) {
+    return pixels1.Color(mappedintensity * (255 * RainbowColor), 255 * RainbowColor, 0);
+  } else if (RainbowColor < 100) {
+    return pixels1.Color(mappedintensity * (255 * RainbowColor), 0, 255 * RainbowColor);
   } else {
-   return pixels1.Color(0, mappedintensity*RainbowColor * 3, mappedintensity*(255-RainbowColor * 3));
-  
+    return pixels1.Color(0, 255 * RainbowColor, 255 * RainbowColor);
+
   }
 
 }

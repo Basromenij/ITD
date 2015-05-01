51aec7b54f7ab03a3bfe9c6096e687e8121956e2
Basromenij <b.ter.haar.romenij@gmail.com>
2015-05-01 11:35:44 +0200

    A fresh start, super lamp telt af wanneer schakelaar is ingedrukt

---
+++ Neopix_freshstart/_1.0/_1.0.ino
@@ -0,0 +1,74 @@
+
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+#define PIN1            3
+#define PIN2            5
+#define teller          8
+#define druk            2
+
+#define NUMPIXELS1      4
+#define NUMPIXELS2      3
+
+boolean status_energy = 1;
+int stepSize = 1;
+int x = 0;
+
+
+float fadeValue = 150;
+
+Adafruit_NeoPixel pixels1 = Adafruit_NeoPixel(NUMPIXELS1, PIN1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(NUMPIXELS2, PIN2, NEO_GRB + NEO_KHZ800);
+
+int delayval = 50; // delay for half a second
+
+void setup() {
+
+  pinMode ( 6, INPUT_PULLUP);
+  pinMode (teller, INPUT);
+  pinMode (druk, INPUT);
+  //  pinMode (schakel, INPUT);
+
+  pixels1.begin(); // This initializes the NeoPixel library.
+  pixels2.begin(); // This initializes the NeoPixel libr nary.
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (digitalRead(druk) == HIGH) {
+    Serial.println("poepchineeeees");
+    status_energy = 1;
+  }
+
+  if (status_energy == 1) {
+
+    if (digitalRead(druk) == HIGH) {
+      for (fadeValue ; fadeValue > 0; fadeValue -= stepSize) {
+        x = fadeValue;
+        Serial.print("waarde van x: ");
+        Serial.println(x);
+        Serial.println(digitalRead(druk));
+        pixels1.setPixelColor(0, pixels1.Color(x, x, x)); // sets a white color for neopix with a brightness x
+        pixels1.setPixelColor(1, pixels1.Color(x, x, x)); // sets a white color for neopix with a brightness x
+        pixels1.setPixelColor(2, pixels1.Color(x, x, x)); // sets a white color for neopix with a brightness x
+        pixels2.setPixelColor(0 , 0); // turns off neopix
+        pixels2.setPixelColor(1 , 0); // turns off neopix
+        pixels2.setPixelColor(2 , 0 ); // turns off neopix
+        pixels1.show(); // This sends the updated pixel color to the hardware.
+        pixels2.show(); // This sends the updated pixel color to the hardware.
+
+        if (x == 0) {
+          status_energy = 0 ;
+          break;
+          Serial.println("waarde is nu een");
+        }
+        if ((digitalRead(druk)) == LOW)  {     // turn other led strip on
+          Serial.print(" hij komt in de break if");
+          break;
+        }
+        delay(delayval); // Delay for a period of time (in milliseconds).
+      }
+    }
+  }
+}

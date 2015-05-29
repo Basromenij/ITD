7691850b5787815b67fc8bae1a394a9045914edf
Basromenij <b.ter.haar.romenij@gmail.com>
2015-05-29 13:33:28 +0200

    Revert 6755f78..13c2185
    This rolls back to commit 6755f78b61fb1de60f958e406f7544672f7a9702.

--- Neopix_freshstart/_1.0/_1.0.ino
+++ Neopix_freshstart/_1.0/_1.0.ino
@@ -19,14 +19,14 @@ int r =                   0;
 int g =                   0;
 int b =                   0;
 
-int Brightness =          255;
+int Brightness =          500;
 int resolutie   =         50;
 
 int RainbowColor =       0;
 int ColorChanger =       1;
 
 float fadeValue =        150;
-int maxx =      150;
+int maxbrightness =      150;
 
 Adafruit_NeoPixel pixels1 = Adafruit_NeoPixel(NUMPIXELS1, PIN1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(NUMPIXELS2, PIN2, NEO_GRB + NEO_KHZ800);
@@ -81,7 +81,6 @@ void loop() {
     if (digitalRead(druk) == HIGH && digitalRead(schakelaar) == LOW) { // SPECIAL RAINBOW
       for (fadeValue ; fadeValue > 0; fadeValue -= stepSize) {
         x = fadeValue;
-
         for (int i = 0; i < 3; i++) {
           pixels1.setPixelColor(i, Wheel(x));
           pixels2.setPixelColor(i, 0); // sets a white color for neopix with a brightness x
@@ -97,103 +96,103 @@ void loop() {
         if ((digitalRead(druk)) == LOW)  {     // turn other led strip on
           Serial.print(" hij komt in de break if");
           break;
+        }          
+          delay(delayval2);
         }
-        delay(delayval2);
       }
     }
-  }
 
-  if (digitalRead(druk) == LOW) {
-    x = fadeValue;
-    for (int i = 0; i < 3; i++) {
-      pixels1.setPixelColor(i , 0); // turns off neopix
-      pixels2.setPixelColor(i, pixels2.Color(x, x, x)); // sets a white color for neopix with a brightness x
-      pixels1.show(); // This sends the updated pixel color to the hardware.
-      pixels2.show(); // This sends the updated pixel color to the hardware.
-    }
+    if (digitalRead(druk) == LOW) {
+      x = fadeValue;
+      for (int i = 0; i < 3; i++) {
+        pixels1.setPixelColor(i , 0); // turns off neopix
+        pixels2.setPixelColor(i, pixels2.Color(x, x, x)); // sets a white color for neopix with a brightness x
+        pixels1.show(); // This sends the updated pixel color to the hardware.
+        pixels2.show(); // This sends the updated pixel color to the hardware.
+      }
 
-    telwaarde = digitalRead(teller);
-    if (telwaarde == HIGH) {
-      for (fadeValue ; fadeValue <= maxx ; Brightness += 1) {
-        Serial.print("hij is aan het tellen");
-        fadeValue ++;
-        break;
+      telwaarde = digitalRead(teller);
+      if (telwaarde == HIGH) {
+        for (fadeValue ; fadeValue <= maxbrightness ; Brightness += 1) {
+          Serial.print("hij is aan het tellen");
+          fadeValue ++;
+          break;
+        }
       }
     }
+    Serial.print("waarde van x: ");
+    Serial.println(x);
   }
-  Serial.print("waarde van x: ");
-  Serial.println(x);
-}
-// changing color of the rainbow
+  // changing color of the rainbow
 
 
-uint32_t Wheel(byte intensity) {
+  uint32_t Wheel(byte intensity) {
 
-  RainbowColor = RainbowColor + ColorChanger;
+    RainbowColor = RainbowColor + ColorChanger;
 
-  if (RainbowColor == 150) {
-    RainbowColor = 0 ;
+    if (RainbowColor == 150) {
+      RainbowColor = 0 ;
+    }
+
+
+    float mappedintensity = map(intensity, 0, maxbrightness, 0, 100) * 0.01;
+
+    int MappedRainbowColor1 = map(RainbowColor, 0, resolutie , 0, Brightness);
+    int MappedRainbowColor2 = map(RainbowColor, resolutie + 1 , resolutie * 2 + 1, Brightness, 0);
+    int MappedRainbowColor3 = map(RainbowColor, resolutie * 2 + 2 , resolutie * 3 + 2, 0, Brightness);
+    int MappedRainbowColor4 = map(RainbowColor, resolutie * 3 + 3 , resolutie * 4 + 3, Brightness, 0);
+    int MappedRainbowColor5 = map(RainbowColor, resolutie * 4 + 4 , resolutie * 5 + 4, 0, Brightness);
+    int MappedRainbowColor6 = map(RainbowColor, resolutie * 5 + 5 , resolutie * 6 + 5, Brightness, 0);
+
+    Serial.print("mappedintesnity");
+    Serial.print(mappedintensity);
+    Serial.print("       ");
+    Serial.print("RainbowColor");
+    Serial.print(RainbowColor);
+    Serial.print("       ");
+    Serial.print("r");
+    Serial.print(r);
+    Serial.print("       ");
+    Serial.print("g");
+    Serial.print(g);
+    Serial.print("       ");
+    Serial.print("b");
+    Serial.println(b);
+
+
+
+
+    if (RainbowColor <= resolutie + 1) {
+      r = Brightness;
+      g = 0;
+      b = MappedRainbowColor1;
+      return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity);
+    } if (RainbowColor <= resolutie * 2 + 2) {
+      r = MappedRainbowColor2;
+      g = 0;
+      b = Brightness;
+      return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity );
+    } if (RainbowColor <= resolutie * 3 + 3) {
+      r = 0;
+      g = MappedRainbowColor3;
+      b = Brightness;
+      return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity);
+    }
+    if (RainbowColor <= resolutie * 4 + 4) {
+      r = 0;
+      g = Brightness;
+      b = MappedRainbowColor4;
+      return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity);
+    } if (RainbowColor <= resolutie * 5 + 5) {
+      r = MappedRainbowColor5;
+      g = Brightness;
+      b = 0;
+      return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity );
+    } if (RainbowColor <= resolutie * 6 + 5) {
+      r = Brightness;
+      g = MappedRainbowColor6;
+      b =  0;
+      return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity);
+    }
+
   }
-
-
-  float mappedintensity = map(intensity, 0, maxx, 0, 100) * 0.01;
-  // 0 - 6e deel van alle telwaardes     0 - 42
-  int MappedRainbowColor1 = map(RainbowColor, 0, 24 , 0, resolutie);
-  int MappedRainbowColor2 = map(RainbowColor, 25 , 50, resolutie + 1 , resolutie * 2 + 1);
-  int MappedRainbowColor3 = map(RainbowColor, 51 , 75, resolutie * 2 + 2 , resolutie * 3 + 2 );
-  int MappedRainbowColor4 = map(RainbowColor, 76, 100, resolutie * 3 + 3 , resolutie * 4 + 3);
-  int MappedRainbowColor5 = map(RainbowColor, 101, 125, resolutie * 4 + 4 , resolutie * 5 + 4);
-  int MappedRainbowColor6 = map(RainbowColor, 126, 150, resolutie * 5 + 5 , resolutie * 6 + 5);
-
-  Serial.print("mappedintesnity");
-  Serial.print(mappedintensity);
-  Serial.print("       ");
-  Serial.print("RainbowColor");
-  Serial.print(RainbowColor);
-  Serial.print("       ");
-  Serial.print("r");
-  Serial.print(r);
-  Serial.print("       ");
-  Serial.print("g");
-  Serial.print(g);
-  Serial.print("       ");
-  Serial.print("b");
-  Serial.println(b);
-
-
-
-
-  if (RainbowColor <= resolutie + 1) {
-    r = Brightness;
-    g = 0;
-    b = MappedRainbowColor1;
-    return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity);
-  } if (RainbowColor <= resolutie * 2 + 2) {
-    r = MappedRainbowColor2;
-    g = 0;
-    b = Brightness;
-    return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity );
-  } if (RainbowColor <= resolutie * 3 + 3) {
-    r = 0;
-    g = MappedRainbowColor3;
-    b = Brightness;
-    return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity);
-  }
-  if (RainbowColor <= resolutie * 4 + 4) {
-    r = 0;
-    g = Brightness;
-    b = MappedRainbowColor4;
-    return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity);
-  } if (RainbowColor <= resolutie * 5 + 5) {
-    r = MappedRainbowColor5;
-    g = Brightness;
-    b = 0;
-    return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity );
-  } if (RainbowColor <= resolutie * 6 + 5) {
-    r = Brightness;
-    g = MappedRainbowColor6;
-    b =  0;
-    return pixels1.Color(r * mappedintensity, g * mappedintensity, b * mappedintensity);
-  }
-
-}

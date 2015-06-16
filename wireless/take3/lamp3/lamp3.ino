1ca024492af431067586076ecfaeffb182e8a9ce
Basromenij <b.ter.haar.romenij@gmail.com>
2015-06-16 10:58:27 +0200

    sessie joost

--- wireless/take3/lamp3/lamp3.ino
+++ wireless/take3/lamp3/lamp3.ino
@@ -4,6 +4,9 @@
 #define PIN1              3
 #define druk1             2
 #define druk2             4
+#define led1              5
+#define led2             6
+
 #define NUMPIXELS1        60
 
 Adafruit_NeoPixel pixels1 = Adafruit_NeoPixel(NUMPIXELS1, PIN1, NEO_GRB + NEO_KHZ800);
@@ -11,8 +14,9 @@ Adafruit_NeoPixel pixels1 = Adafruit_NeoPixel(NUMPIXELS1, PIN1, NEO_GRB + NEO_KH
 uint8_t x = 0;
 int stand2 = 0;
 int stand1 = 0;
-
-int sendData[] = { 0, 0, 0};
+int currentState = 0;
+int switch0 = 0;
+int switch1 = 0;
 int delayval = 50;
 
 void setup() {
@@ -25,55 +29,84 @@ void setup() {
 }
 
 void loop() {
-  sendData[0] = digitalRead(druk1);
-  sendData[1] = digitalRead(druk2);
-  //  sendData[2] = x;
 
-  stand1 = digitalRead(druk1);
-  stand2 = digitalRead(druk2);
+  switch0 = digitalRead(druk1);
+  switch1 = digitalRead(druk2);
 
+  if (switch0 == 0  && currentState != 0) {
+    analogWrite(led1, 255);
+    analogWrite(led2, 255);
 
-  if (Serial.available() > 0) { // check if communciation is available
-
-
-    x = Serial.read();   // read the serial communicated value and assign this to x
-
-    if (stand1 == 1 && stand2 == 0) {
-      //        x --;
-      for (int i = 0; i < 60; i++) {
-        pixels1.setPixelColor(i, 0, 0, x);  // shine blue light when just switch one is high
-      }
-      for (int i = 0; i < 2; i++) {
-        Serial.write(sendData[i]);
-      }
-      delay(delayval); // Delay for a period of time (in milliseconds).
-    }
-
-
-    if (stand1 == 1 && stand2 == 1) {  // shine red light when both switches are high
-      //        x --;
-      for (int i = 0; i < 60; i++) {
-        pixels1.setPixelColor(i, x , 0 , 0);
-      }
-      for (int i = 0; i < 2; i++) {
-        Serial.write(sendData[i]);
-      }
-      delay(delayval); // Delay for a period of time (in milliseconds).
-    }
-
-
-    if (stand1 == 0  && stand2 == 0) {  // shine red light when both switches are high
-      for (int i = 0; i < 60; i++) {
-        pixels1.setPixelColor(i, 0 , 0 , 0);
-      }
-      for (int i = 0; i < 2; i++) {
-        Serial.write(sendData[i]);
-      }
-      delay(delayval); // Delay for a period of time (in milliseconds).
-    }
-
-    pixels1.show();
+    Serial.println(0);
+    currentState = 0;
+    delay(100);
   }
+
+  if (switch0 == 1 && switch1 == 0 && currentState != 1) {
+
+    analogWrite(led1, 255);
+    analogWrite(led2, 0);
+
+    Serial.println(1);
+    currentState = 1;
+    delay(100);
+
+  }
+
+  if (switch0 == 1  && switch1 == 1 && currentState != 2 ) {
+
+    analogWrite(led2, 255);
+    analogWrite(led1, 0);
+
+    Serial.println(2);
+    currentState = 2;
+    delay(100);
+
+  }
+
+
+
+  //  if (Serial.available() > 0) { // check if communciation is available
+  //
+  //
+  //    x = Serial.read();   // read the serial communicated value and assign this to x
+  //
+  //    if (stand1 == 1 && stand2 == 0) {
+  //      //        x --;
+  //      for (int i = 0; i < 60; i++) {
+  //        pixels1.setPixelColor(i, 0, 0, x);  // shine blue light when just switch one is high
+  //      }
+  //      for (int i = 0; i < 2; i++) {
+  //        Serial.write(sendData[i]);
+  //      }
+  //      delay(delayval); // Delay for a period of time (in milliseconds).
+  //    }
+  //
+  //
+  //    if (stand1 == 1 && stand2 == 1) {  // shine red light when both switches are high
+  //      //        x --;
+  //      for (int i = 0; i < 60; i++) {
+  //        pixels1.setPixelColor(i, x , 0 , 0);
+  //      }
+  //      for (int i = 0; i < 2; i++) {
+  //        Serial.write(sendData[i]);
+  //      }
+  //      delay(delayval); // Delay for a period of time (in milliseconds).
+  //    }
+  //
+  //
+  //    if (stand1 == 0  && stand2 == 0) {  // shine red light when both switches are high
+  //      for (int i = 0; i < 60; i++) {
+  //        pixels1.setPixelColor(i, 0 , 0 , 0);
+  //      }
+  //      for (int i = 0; i < 2; i++) {
+  //        Serial.write(sendData[i]);
+  //      }
+  //      delay(delayval); // Delay for a period of time (in milliseconds).
+  //    }
+  //
+  //    pixels1.show();
+  //  }
 }
 
 
--- wireless/take3/watch3/watch3.ino
+++ wireless/take3/watch3/watch3.ino
@@ -1,5 +1,7 @@
 const int tilt = 7;
-const int led = 6;
+const int led1 = 6;
+const int led2 = 3;
+int currentState = 0;
 uint8_t x = 0;
 
 // boolean status_energy =   1;
@@ -7,20 +9,75 @@ uint8_t x = 0;
 void setup() {
   Serial.begin(9600);
   pinMode(tilt, INPUT);
-  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
 void loop() {
+  if (Serial.available() > 0) {
 
-  if ((digitalRead (tilt) == HIGH)) {
-    x++;
-    Serial.write(x);
-    analogWrite(led, x);
-    if (x == 255) {
-      x = 0;
+    currentState = Serial.parseInt();
+
+    if (currentState == 0) {
+      analogWrite(led1, 0);
+      analogWrite(led2, 255);
     }
-    delay(10);
+
+    if (currentState == 1 || currentState == 2) {
+      analogWrite(led1, 255);
+      analogWrite(led2, 0);
+    }
+
   }
+
+
+if ((digitalRead (tilt) == HIGH) && currentState == 0) {
+counter++;  
+Serial.println(counter);
 }
+
+//    int standschakelaar1 = Serial.parseInt();
+//    int standschakelaar2 = Serial.parseInt();
+//    //    int x = Serial.parseInt();
+//
+//    //  int telwaarde = Serial.parseInt();
+//    if (Serial.read() == '\n') {
+//      if (standschakelaar1 == 0 && standschaklelaar2 == 0 ) {
+//
+//        if (firstFlag == 1 ) {
+//          counter = x;
+//          firstFlag = 0;
+//        }
+//        counter++;
+//      }
+//
+//
+//
+//
+//      if (standschakelaar1 == 1)
+//      {
+//        analogWrite(led1, 255);
+//        firstFlag = 1;
+//      }
+//
+//      if (standschakelaar2 == 1)
+//      {
+//        analogWrite(led1, 0);
+//        analogWrite(led2, 255);
+//      }
+//    }
+//  }
+//}
+
+//  if ((digitalRead (tilt) == HIGH)) {
+//    x++;
+//    Serial.write(x);
+//    analogWrite(led, x);
+//    if (x == 255) {
+//      x = 0;
+//    }
+//    delay(10);
+//  }
+//}
 
 
